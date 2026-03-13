1f104cd0704dbbd6bcb9cd83903c45b7494fc9fa
sohila-nabil <nsohila03@gmail.com>
2026-03-13 21:27:51 +0200

    Add mongodb and save users from clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,21 +1,54 @@
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
+import { createOrUpdateUser, deleteUser } from "../../../../lib/services/user";
+import { clerkClient } from "@clerk/nextjs/dist/types/server";
 
 export async function POST(req) {
   try {
     const evt = await verifyWebhook(req);
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`,
-    );
-    console.log("Webhook payload:", evt.data);
+    if (evt.type === "user.created" || evt.type === "user.updated") {
+      const {
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username,
+      } = evt.data;
+      try {
+        const user = await createOrUpdateUser(
+          id,
+          first_name,
+          last_name,
+          image_url,
+          email_addresses,
+          username,
+        );
+        if (user && evt.type === "user.created") {
+          try {
+            await clerkClient.users.updateUserMetadata(id, {
+              publicMetadata: {
+                userMongoId: user._id,
+                isAdmin: user.isAdmin,
+              },
+            });
+          } catch (error) {
+            console.log("updateUserMetadata", error);
+          }
+        }
+      } catch (error) {
+        console.log("Webhook with create user error", error);
+      }
+    }
 
-    if (evt.type === "user.created") {
-      console.log("userId:", evt.data.id);
+    if (evt.type === "user.deleted") {
+      const { id } = evt.data;
+      try {
+        await deleteUser(id);
+      } catch (error) {
+        console.log("delete user from route webhook", error);
+      }
     }
 
     return new Response("Webhook received", { status: 200 });
