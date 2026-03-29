13a838e54847ec4daad3bac2fecdb157f3d7addf
sohila-nabil <nsohila03@gmail.com>
2026-03-29 18:55:22 +0200

    update env

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,12 +1,10 @@
 import BlogList from "../../../components/BlogList";
 import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
-// import Navbar from "../../../components/Navbar";
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
       <Header />
       <BlogList />
       <Footer />
