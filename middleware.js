db5486b98f049eb7d0710fff8b2ab2aea04a24a4
sohila-nabil <nsohila03@gmail.com>
2026-03-29 18:07:28 +0200

    Add Medthod

--- middleware.js
+++ middleware.js
@@ -3,14 +3,13 @@ import { NextResponse } from "next/server";
 export function middleware(req) {
   const res = NextResponse.next();
 
-  // السماح لدومينك فقط
   res.headers.set(
     "Access-Control-Allow-Origin",
     "https://next-blog-app-d6un.vercel.app",
   );
   res.headers.set(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
+    "GET, POST, PUT, DELETE, OPTIONS","PATCH"
   );
   res.headers.set("Access-Control-Allow-Headers", "Content-Type");
 
